355dfeeed20ce4f6b23ff4bf6a3e15fd45002386
Xander Gilbert <Xanderjamesgilbert@gmail.com>
2026-07-09 21:59:05 -0600

    gardenmngnt

---
+++ index.js
@@ -0,0 +1,3 @@
+let favoriteNumber=12; if (favoriteNumber >=10) { console.log("My favorite number is greater than or equal to 10."); } else { console.log("My favorite number is less than 10."); 
+        
+}
